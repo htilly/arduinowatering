f809d2309e398dbf8193b6f68f0ceb72c8f085ca
hentil <henrik.tilly@schibsted.se>
2015-08-17 09:33:33 +0200

    Added libraries and updated code

--- Bevattning/Bevattning.ino
+++ Bevattning/Bevattning.ino
@@ -1,16 +1,43 @@
-// #include <Ethernet.h>
 #include <SPI.h>
-// #include <aJSON.h>
 #include <SimpleTimer.h>
-// #include <sha1.h>
+#include <dht.h>
+#define dht_apin A0
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+#include "Wireless.h"
 
+dht DHT;
+
+//You will need to modify the next 3 lines to match you WiFi network and you ThingSpeak account.
+// NOTE - For EPS8266 to work you NEED to modify SoftwareSerial.h.
+// Make sure it is: #define _SS_MAX_RX_BUFF 256 // RX buffer size
+// For OSX, this is the file:
+// /Applications/Arduino.app/Contents/Java/hardware/arduino/avr/libraries/SoftwareSerial/SoftwareSerial.h
+
+const String APSSID = ""; //The name of the AP/SSID you connect to
+const String wpaKey = ""; //The WPA key of your AP
+const String ThingSpeakKey = ""; //You get this key when you sign up for a ThingSpeak account.
+
+
+
+
+Wireless wifi(6,7); //Set up the WiFi library. Parameters: RX pin, TX pin, Enable pin, Reset pin. Normally: 10,4,2,3
 
 
 /*
   Pin configuration
 */
-int moistureInputPin = 0;
-int valveOutputPin = 8;
+
+//Funduino: Soil Moisture Sensor Ground Humidity Sensor Module
+// http://www.miniinthebox.com/soil-moisture-sensor-ground-humidity-sensor-module_p903362.html
+
+// int moistureInputPin = 0;
+int moistureInputPin = A0;
+int Temp = A1;
+
+//Water valve
+// https://www.sparkfun.com/products/10456
+int valveOutputPin = 9;
 
 
 
@@ -20,36 +47,24 @@ int valveOutputPin = 8;
 */
 unsigned long pollMillis = 1800000; // 30 min
 
+//unsigned long pollMillis = 30000; // 1 min
 
-/* Shall be 700
-Testing with 800 
-
+/*
 Calebrating..
 300 - Dry dirt !
 700 OK
 Finally - set it to 650
 */
-int waterAtMoisture = 650;
+
+int waterAtMoisture = 400;
 
 /*
   When the soil gets dry enough to warrant a watering,
   how many millis to open the valve for.
 */
-int waterForMillis = 3500; // 3,5 sconds
+int waterForMillis = 1500; // 3,5 sconds
 
 
-/*
-  Local IP settings
-
-
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-byte ip[] = { 172, 16, 200, 138 };
-byte submask[] = { 255, 255, 255, 0 };
-byte gateway[] = { 172, 16, 200, 1 };
-byte dns[] = { 8, 8, 8, 8 };
-
-EthernetClient client;
-*/
 
 SimpleTimer timer;
 
@@ -58,20 +73,32 @@ SimpleTimer timer;
 void setup() {
   pinMode(valveOutputPin, OUTPUT);
 
+
+
+
   Serial.begin(9600);
-  
-  Ethernet.begin(mac, ip, dns, gateway, submask);
+  Serial.println("Connecting to WiFi.");
+  debug(4);; //The WiFi module need a little time to start, so we will wait before connecting to AP.
 
-    Serial.print("My IP address: ");
-    Serial.println(Ethernet.localIP());
+  wifi.connectToAP(APSSID, wpaKey); //Connects to the access point. The WiFi module will remember this network an connect automatically next time.
+  debug(4);; //Wait a little to make sure it has time to connect.
 
-
+  Serial.println("Checking IP.");
+  wifi.getIP(); //Check if got an IP.
+  debug(8);
+  wifi.getStatus(); //Get the connection status.
+  debug(8);
+  Serial.println("Ready...");
 
   timer.setInterval(pollMillis, poll);
 }
 
 void loop() {
+
+  // DHT.read11(dht_apin);
+  DHT.read11(Temp);
   timer.run();
+
 }
 
 /*
@@ -80,18 +107,55 @@ void loop() {
 void poll() {
   Serial.print("Polling... ");
 
+  Serial.print("Humidity & Temperature:");
+  Serial.print("Current humidity = ");
+  Serial.print(DHT.humidity);
+  Serial.print("%  ");
+  Serial.print("temperature = ");
+  Serial.print(DHT.temperature);
+  Serial.println("C  ");
+  Serial.println();
+  Serial.println();
+  Serial.println();
+
+
   //Using int for watered rather than boolean
   //as the cube server doesn't report metrics
   //on booleans very well
   int moisture = getSoilMoisture();
   int watered = 0;
-
+  Serial.print("Moisture:");
   Serial.println(moisture);
+  Serial.println();
+  Serial.println();
+  Serial.print("----- END -----");
+  Serial.println();
+  Serial.println();
 
   if (moisture < waterAtMoisture) {
     waterThePlant();
     watered = 1;
   }
+
+  //Send data to server
+  String request = "";
+  request = request + "GET /update?key=" + ThingSpeakKey + "&field1=" + DHT.humidity + "&field2=" + DHT.temperature + "&field3=" + moisture + "&field4=" + watered + "\r\n";
+
+  Serial.println("Connecting to server.");
+  wifi.connectToServer("184.106.153.149", 80); //Connects to the Thingspeak server on port 80.
+  debug(4);
+
+  Serial.println("Sending request.");
+  wifi.sendDataToServer(request);  //Sends the GET request.
+  debug(10); //Wait 20 seconds
+
+  Serial.println("Disconnecting from server.....");
+  wifi.disconnectFromServer();
+  debug(20);
+
+  Serial.println("Done!");
+  Serial.println();
+  Serial.println("Going to sleep.");
 
 
 }
@@ -112,10 +176,20 @@ int getSoilMoisture() {
   Gives the plant a blast of water
 */
 void waterThePlant() {
+  Serial.println();
+  Serial.println();
   Serial.println ("Watering... ");
+  Serial.println();
   digitalWrite(valveOutputPin, HIGH);
   delay(waterForMillis);
   digitalWrite(valveOutputPin, LOW);
 }
 
-
+void debug(int loops) {
+  for (int i = 0; i <= loops; i++) {
+    while (wifi.available()) {
+      Serial.write(wifi.readChar());
+    }
+    delay(1000);
+  }
+}
--- README.md
+++ README.md
@@ -5,7 +5,25 @@ Simple solution for watering my plants....
 The following HW is beeing used:
 
 -	Arduino Uno
--	Arduino Relay Shield
 -	Moisture Sensor, http://www.miniinthebox.com/sv/fuktighetssensor-ground-luftfuktighet-sensor-module_p903362.html
 -	Magnetic Valve, https://www.sparkfun.com/products/10456
--	
+
+NOTE:
+
+Libraries in both original Arduino and WiFi has been modified in order to get this to work.
+
+You will need to modify the next 3 lines to match you WiFi network and you ThingSpeak account.
+NOTE - For EPS8266 to work you NEED to modify SoftwareSerial.h.
+Make sure it is: #define _SS_MAX_RX_BUFF 256 // RX buffer size
+For OSX, this is the file:
+/Applications/Arduino.app/Contents/Java/hardware/arduino/avr/libraries/SoftwareSerial/SoftwareSerial.h	
+
+
+Problem right now:
+Moisture Sensor get broken within a week. Problem is that it´s constantly getting 5V causing corrosion.
+To solve the problem I will power the sensor thru a relay.
+
+
+TODO:
+-	Power Moisture Sensor thru relay
+-	Pull setting from external source
